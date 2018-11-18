3bcce5779b18622a0c80907ec4f576979d5dde0a
JunyiZHONG <zhongjunyi.dev@gmail.com>
2018-11-19 00:21:14 +0100

    tick on Nested Loop Structure 005 plus

--- src/TestNestedLoop.java
+++ src/TestNestedLoop.java
@@ -23,14 +23,31 @@ public class TestNestedLoop {
         //用while循环分别计算100以内的奇数以及偶数的和，并输出
         int SumOddNumber =0;
         int SumEvenNumber = 0;
-        for(int i=1;i<=100;i++){
-            if(i%2==0){
-                SumEvenNumber +=1;
-            }else {
-                SumOddNumber +=i;
+        for(int i=1;i<=100;i++) {
+            if (i % 2 == 0) {
+                SumEvenNumber += i;
+            } else {
+                SumOddNumber += i;
             }
-            System.out.println("奇数和："+SumOddNumber);
-            System.out.println("偶数和："+SumEvenNumber);
+        }
+        System.out.println("奇数和："+SumOddNumber);
+        System.out.println("偶数和："+SumEvenNumber);
+
+        System.out.println("******************************");
+        //用while循环或者其他循环输出1-1000之间能被5整除的数，且每行输出5个
+        int k=0;
+        for (int j =1;j<=1000;j++){
+            if (j%5==0){
+                System.out.print(j+"\t");
+                k++;
+            }
+//            if (j%25==0){
+//                System.out.println();
+//            }
+            if(k==5){
+                System.out.println();
+                k=0;
+            }
+        }
         }
     }
-}
